e4434012bd1f10ec570161ba7c1aa3d20ee716e0
eifo1 <averyalexandercarroll08@gmail.com>
2023-05-14 21:33:08 -0400

    authentication controller and routes

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,13 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dioxide</title>
     <link rel="stylesheet" href="/css/app.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-4">
+                <h1 class="text-center">
+                    Dioxide
+                </h1>
+            </div>
+            <div class="col-4">
+                <h1 class="text-center">
+                    Login
+                </h1>
+            </div>
+            <div class="col-4">
+                <h1 class="text-center">
+                    Register
+                </h1>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-3">
+                <p>
+                    A <dfn><abbr title="Free and Open Source Software">FOSS</abbr></dfn> platform powering the future of social media.
+                </p>
+            </div>
+            <div class="col-3">
+                form
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
